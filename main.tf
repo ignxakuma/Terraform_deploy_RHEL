55be78c5ae2bb8bead1f308e18d0cb963ee968d4
Bhavesh Parab <bhaveshparab9821@gmail.com>
2023-04-20 15:49:13 +0530

    Terraform deply Azure RHEL VM

---
+++ main.tf
@@ -0,0 +1,195 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.52.0"
+    }
+  }
+}
+
+locals {
+  tags= {
+    Env = "Dev"
+  }
+}
+
+provider "azurerm" {
+  subscription_id = "8536ea4c-0a5d-4f33-9058-7f851db30173"
+  tenant_id = "b9325d2b-9688-4c2e-b137-a732c58109d5"
+  client_id = "e08c3b54-0760-4fb4-b5ec-8b38310a3109"
+  client_secret = "EN38Q~zVWIcZGa.HaRk6mB6Sno~4aUswhyUdda-i"
+  features {}
+}
+
+resource "azurerm_resource_group" "Terrarg" {
+    name = "terrarg"
+    location = "east us"  
+    tags = {
+      "Env" = "Dev"
+    }
+}
+
+resource "azurerm_storage_account" "Terrastorage171998" {
+    name = "terrastorage171998"
+    resource_group_name = azurerm_resource_group.Terrarg.name
+    location = azurerm_resource_group.Terrarg.location
+    account_tier = "Standard"
+    account_replication_type = "LRS"
+    tags = {
+      "Env" = "Dev"
+    }
+    account_kind = "StorageV2"
+}
+
+resource "azurerm_virtual_network" "terravnet" {
+  name                = "terravnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.Terrarg.location
+  resource_group_name = azurerm_resource_group.Terrarg.name
+  tags = local.tags
+}
+
+resource "azurerm_subnet" "terrasubnet" {
+  name                 = "Appsrvers"
+  resource_group_name  = azurerm_resource_group.Terrarg.name
+  virtual_network_name = azurerm_virtual_network.terravnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "terra-pub-ip" {
+  name                = "terra-pub-ip"
+  resource_group_name = azurerm_resource_group.Terrarg.name
+  location            = azurerm_resource_group.Terrarg.location
+  allocation_method   = "Static"
+  tags = local.tags
+}
+
+resource "azurerm_network_security_group" "example" {
+  name                = "example-nsg"
+  location            = azurerm_resource_group.Terrarg.location
+  resource_group_name = azurerm_resource_group.Terrarg.name
+}
+
+resource "azurerm_network_security_rule" "inbound-ssh" {
+  name                        = "allow-ssh-inbound"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  network_security_group_name = azurerm_network_security_group.example.name
+  resource_group_name = azurerm_resource_group.Terrarg.name
+  depends_on = [azurerm_network_security_group.example]
+}
+
+resource "azurerm_network_security_rule" "inbound-http" {
+  name                        = "allow-http-inbound"
+  priority                    = 101
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  network_security_group_name = azurerm_network_security_group.example.name
+  resource_group_name = azurerm_resource_group.Terrarg.name
+  depends_on = [azurerm_network_security_group.example]
+}
+
+resource "azurerm_network_security_rule" "inbound-https" {
+  name                        = "allow-https-inbound"
+  priority                    = 102
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "443"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  network_security_group_name = azurerm_network_security_group.example.name
+  resource_group_name = azurerm_resource_group.Terrarg.name
+  depends_on = [azurerm_network_security_group.example]
+}
+
+resource "azurerm_network_security_rule" "inbound-all" {
+  name                        = "allow-all-inbound"
+  priority                    = 200
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  network_security_group_name = azurerm_network_security_group.example.name
+  resource_group_name = azurerm_resource_group.Terrarg.name
+  depends_on = [azurerm_network_security_group.example]
+}
+
+resource "azurerm_network_security_rule" "outbound-all" {
+  name                        = "allow-all-outbound"
+  priority                    = 100
+  direction                   = "Outbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  network_security_group_name = azurerm_network_security_group.example.name
+  resource_group_name = azurerm_resource_group.Terrarg.name
+  depends_on = [azurerm_network_security_group.example]
+}
+
+resource "azurerm_network_interface_security_group_association" "terransg" {
+  network_interface_id = azurerm_network_interface.terranic.id
+  network_security_group_id = azurerm_network_security_group.example.id
+}
+
+resource "azurerm_network_interface" "terranic" {
+  name                = "terranic"
+  location            = azurerm_resource_group.Terrarg.location
+  resource_group_name = azurerm_resource_group.Terrarg.name
+
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.terrasubnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.terra-pub-ip.id
+    }
+  tags = local.tags
+}
+
+resource "azurerm_linux_virtual_machine" "terra-docker-vm" {
+  name                = "terra-docker-vm"
+  resource_group_name = azurerm_resource_group.Terrarg.name
+  location            = azurerm_resource_group.Terrarg.location
+  size                = "Standard_B1s"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.terranic.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDYv0k85GSQpOEsoQBAPVglUjRqz9qI/e1Qq+Q6pBVP36ejqhEI7ReJvXwmWNgTlJdUc4Zya8T7vfXUDWeSWZtzVh8y7Mava0oWenzwneBWzZa2rshs0vlSILE1+Gu5lspY4oxbBqAzvGdhT3DaNkKziz5DVCeO+sA0iYVD4FydsLfSX8NtL5+Zai6bQbRwmi2LZzL2ibsiqcKKVRfoBvQI5/EK2vxSqEOFgPi4jQ0ldXYrQTLFjX/2NApoe6D/dUhQtTWdNc25rhLBUILm8Zes3pcCNterf4jSp5O4bcO8sMNxwAH1ghOgW21r0Mvuil2OkcdU63zyzOG0BIstqaIF rsa-key-20230420"
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+  }
+
+  source_image_reference {
+    publisher = "RedHat"
+    offer     = "RHEL"
+    sku       = "8-gen2"
+    version   = "latest"
+  }
+  tags = local.tags
+}
