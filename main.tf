7fa00126802cf4a9faff8ccccee313f2f3a448e9
ignxakuma <98381213+ignxakuma@users.noreply.github.com>
2023-10-13 11:45:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -162,7 +162,7 @@ resource "azurerm_network_interface" "terranic" {
 }
 
 resource "azurerm_linux_virtual_machine" "terra-docker-vm" {
-  name                = "terra-docker-vm"
+  name                = var.bundle_name+"_"+var.env_name
   resource_group_name = azurerm_resource_group.Terrarg.name
   location            = azurerm_resource_group.Terrarg.location
   size                = "Standard_B1s"
